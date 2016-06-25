00448928322247f9a9f3076337c033305cf89ef2
srikanth <sree2chin@gmail.com>
2016-06-26 02:59:34 +0530

    optimize the space usage

--- index.js
+++ index.js
@@ -4,41 +4,49 @@
 var randomColors = ['#5F9EA0', '#6495ED', '#A9A9A9', '#BDB76B', ' #DCDCDC', '#778899', '#9370DB'];  
 
 window.onload = function() {
-  // assuming the array is sorted with respect to the start time.
+  
   var meetings = [
-	 //  {
-		// id : 1, start : 120, end : 150
-	 //  },
-	  // {
-		 //  id : 2, start : 120, end : 160
-	  // },
-   //  {
-   //    id : 21, start : 200, end : 260
-   //  },
-   //  {
-   //    id : 12, start : 155, end : 165
-   //  },
     {
       id : 1, start : 60, end : 150
     },
-	  {
-		  id : 3, start : 540, end : 570
-	  },
-	  {
-		  id : 4, start : 555, end : 600
-	  },
-	  {
-		  id : 5, start : 585, end : 660
-	  },
     {
-      id : 6, start : 700, end : 710
-    }
+      id : 2, start : 540, end : 570
+    },
+    {
+      id : 3, start : 555, end : 600
+    },
+    {
+      id : 4, start : 585, end : 660
+    },
+    // {
+    //   id : 11, start : 60, end : 120
+    // },
+    // {
+    //   id : 13, start : 80, end : 140
+    // },
+    // {
+    //   id : 14, start : 100, end : 209
+    // },
+    // {
+    //   id : 15, start : 130, end : 180
+    // },
+    // {
+    //   id : 16, start : 155, end : 220
+    // },
+    // {
+    //   id : 17, start : 170, end : 210
+    // },
   ];
-  meetings.sort(function(meetingA, meetingB){
-      return meetingA.start > meetingB.start;
+  // making the array sorted with respect to the start time.
+  meetings.sort(function(keyA, keyB){
+      if(keyA.start < keyB.start) return -1;
+      if(keyA.start > keyB.start) return 1;
+      return 0;
   });
-  var posArray = calculatePositions(meetings);
-  createDivs(posArray);
+  calculatePositions(meetings)
+
+  // var posArray = calculatePositions(meetings);
+  // createDivs(posArray);
 
 };
 
@@ -46,9 +54,20 @@ window.onload = function() {
 function calculatePositions(arrMeeting) {
   	var arrLength = arrMeeting.length;
   	var posArray = [];
-  	var i, j, k, widthRatio, startElem, currentCounter;
+  	var i, 
+        j, 
+        k,
+        x, 
+        widthRatio, 
+        startElem, 
+        currentCounter,
+        groupCounter;
+    var resultArray = [];
+    var resultObj = {};
+    var resultObj2 = {};
   	if(arrLength && arrLength > 0) {
     		for (i = 0; i < arrLength; i++) {
+            //calculate dimensions
             widthRatio = 1;
       			var meetingObj = {};
       			meetingObj.height = (arrMeeting[i].end - arrMeeting[i].start)*2;
@@ -63,9 +82,8 @@ function calculatePositions(arrMeeting) {
         var posArrayLength = posArray.length;
         widthRatio = currentCounter = 1;
         startElem = k = 0;
-        // finding relative width and left position
         while(startElem < posArrayLength && k < posArrayLength) {
-            debugger;
+            resultArray = [];
             // Detecting intersection of meetings.
             if( (k < posArrayLength - 1) && (arrMeeting[k].start < arrMeeting[k+1].end) && (arrMeeting[k].end > arrMeeting[k+1].start)) {
                 posArray[k].widthRatio = widthRatio
@@ -73,68 +91,153 @@ function calculatePositions(arrMeeting) {
                 k++;
                 currentCounter++;
             }
+            /* If there's an intersection find all the adjacent divs which intersect
+             * and adjust them without wasting space
+             */
             else {
+                // Iterating through intersection array.
                 if(k > startElem) {
-                   for(var x = startElem; x < k + 1; x++) {
+                   resultObj = {}
+                   for(x = startElem; x < k + 1; x++) {
                       if(posArray[x]) {
                           posArray[x].width = 600/(currentCounter)
                           posArray[x].left = (posArray[x].width) * (posArray[x].widthRatio - 1);
                       }
                    }
+                   // Initial div in the vertical column
+                   resultObj.posArray = [];
+                   resultObj.posArray.push(posArray[startElem])
+                   resultObj.groupCounter = 0;
+                   resultArray.push(resultObj);
+                   groupCounter = 1;
+                   for(x = startElem + 1; x < k + 1; x++) {
+                      if(posArray[x]) {
+                          resultObj = {}
+                          /* Create vertical columns divs with divs which are overlapping 
+                           * with the beginning div
+                           */
+                          if( (posArray[startElem].top + posArray[startElem].height) > posArray[x].top) {
+                              resultObj.posArray = [];
+                              posArray[x].left = 0;
+                              resultObj.posArray.push(posArray[x])
+                              resultObj.groupCounter = 0;
+                              resultArray.push(resultObj)
+                          }
+                          else {
+                              var resultArrayLength = resultArray.length;
+                              /* Take the div which is non-overlapping with first div in the
+                               * group and try to make it fit on below the existing divs.
+                               */
+                              for(var a = 0; a < resultArrayLength; a++) {
+                                  if( (resultArray[a].posArray[resultArray[a].groupCounter].top + resultArray[a].posArray[resultArray[a].groupCounter].height) < posArray[x].top ) {
+                                      resultArray[a].posArray.push(posArray[x]);
+                                      resultArray[a].groupCounter = resultArray[a].groupCounter + 1;
+                                      break;
+                                  }
+                              }
+                              /*if a div doesn't fit in the existing vertical columns
+                               *create another one
+                               */
+                              if(a >= resultArrayLength) {
+                                  resultObj2 = {};
+                                  resultObj2.posArray = [];
+                                  resultObj2.posArray.push(posArray[x]);
+                                  resultObj2.groupCounter = 0;
+                                  resultArray.push(resultObj2);
+                              }
+                          }
+                      }
+                   }
                 }
+
                 else {
-                    k++
+                    if(posArray[k]) {
+                       resultObj = {};
+                        resultObj.posArray = [];
+                        resultObj.posArray.push(posArray[startElem])
+                        resultObj.groupCounter = 0;
+                        resultArray.push(resultObj); 
+                    }
                 }
+                k++;
+                renderSpaceOptimizedDivs(resultArray)
+                
                 currentCounter = 1;
                 widthRatio = 1;
                 startElem = k;
             }
+
         }
-        // }
-        /* creating divs can be done here only, just for the code to be clear it is 
-             * moved to another function.
-             */
-    		return posArray;
+    		// return posArray;
   	}
   	else {
-        return posArray;
+        // return posArray;
     		alert("please enter meetings.");
   	}
 }
 
-// fuction to create divs with respect to the given data.
-function createDivs(posArray) {
+// fuction to create divs with optimized space.
+function renderSpaceOptimizedDivs(resultArray) {
     var i,
         j,
         div,
         color,
         div;
-    var posArrayLength = posArray.length;
-    for (i = 0; i < posArrayLength; i++) {
-        color = randomColors[Math.floor(Math.random() * randomColors.length)];
-
-        div = document.createElement('div')
-        $(div).prop('id', posArray[i].id);
-        $(div).append('<p>'+posArray[i].id+'</p>')
-        $(div).prop('class', "innerContainer");
-        $(div).css("background-color", color);
-        width = posArray[i].width + "px";
-        $(div).css("width", posArray[i].width + "px");
-        $(div).css("height", posArray[i].height + "px");
-        $(div).css("top", posArray[i].top + "px");
-        $(div).css("left", posArray[i].left + "px");
-        $(div).css("line-height", posArray[i].height + "px");
-        // $(div).css("position", "absolute");
-        $('.outerContainer').append(div)
+    var resultArrayLength = resultArray.length;
+    for (j = 0; j < resultArrayLength; j++) {
+        var posArray = resultArray[j].posArray;
+        var posArrayLength = posArray.length;
+        for(i = 0; i < posArrayLength; i++) {
+            // assign random color.
+            color = randomColors[Math.floor(Math.random() * randomColors.length)];
+            div = document.createElement('div')
+            $(div).prop('id', posArray[i].id);
+            $(div).append('<p>'+posArray[i].id+'</p>')
+            $(div).prop('class', "innerContainer");
+            $(div).css("background-color", color);
+            // finding width with respect to number of groups
+            width = (600/resultArrayLength);
+            $(div).css("width", width + "px");
+            $(div).css("height", posArray[i].height + "px");
+            $(div).css("top", posArray[i].top + "px");
+            //incrementing left.
+            $(div).css("left", (j * width) + "px");
+            $(div).css("line-height", posArray[i].height + "px");
+            // $(div).css("position", "absolute");
+            $('.outerContainer').append(div)
+        }
+        
         
     }
-    // $(".innerContainer").hover(
-    //     function() {
-    //       $(this).siblings(".innerContainer").addClass("lessOpacity")
-    //     },
-    //     function() {
-    //       $(this).siblings(".innerContainer").addClass("normaOpacity")
-    //     }
-    // )
+
 }
+
+// fuction to create divs with respect to the given data.
+// function createDivs(posArray) {
+//     var i,
+//         j,
+//         div,
+//         color,
+//         div;
+//     var posArrayLength = posArray.length;
+//     for (i = 0; i < posArrayLength; i++) {
+//         color = randomColors[Math.floor(Math.random() * randomColors.length)];
+
+//         div = document.createElement('div')
+//         $(div).prop('id', posArray[i].id);
+//         $(div).append('<p>'+posArray[i].id+'</p>')
+//         $(div).prop('class', "innerContainer");
+//         $(div).css("background-color", color);
+//         width = posArray[i].width + "px";
+//         $(div).css("width", posArray[i].width + "px");
+//         $(div).css("height", posArray[i].height + "px");
+//         $(div).css("top", posArray[i].top + "px");
+//         $(div).css("left", posArray[i].left + "px");
+//         $(div).css("line-height", posArray[i].height + "px");
+//         // $(div).css("position", "absolute");
+//         $('.outerContainer').append(div)
+        
+//     }
+
+// }
 
